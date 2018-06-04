e076d1d302e1a3cc714cb9a5c590ec52c3f6889e
Jake Bright <jbright32@my.stlcc.edu>
2018-06-04 04:22:00 -0500

    ready to turn in!

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,14 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+
+import static org.launchcode.controllers.ListController.columnChoices;
 
 /**
  * Created by LaunchCode
@@ -18,10 +22,42 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
 
-}
+    @RequestMapping(value = "results")
+    public String processResultsForm(Model model, @RequestParam String searchType , @RequestParam String searchTerm){
+        model.addAttribute("columns" , ListController.columnChoices);
+
+        if (searchType.equals("all")) {
+            model.addAttribute("columns" , ListController.columnChoices);
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobs);
+
+            return "search";
+        }
+
+        else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+
+            return "search";
+        }
+
+
+                //call correct jobDate method
+                //return what jobData gives you
+
+            }
+            //return all jobs
+           // return JobData.allJobs;
+        }
+
+        //search by column and value
+        //return what we get
+
+
+
